d10e221743bd89025fc4603832ac52df4727ccd1
Jan Kozel <153216339+WorkLoopC@users.noreply.github.com>
2025-09-30 09:09:09 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    uint8_t counter = 0;
+    DDRD |= (1 << PD5) | (1 << PD4) | (1 << PD3);
+    PORTD |= (1 << PD6);
+    while (1)
+    {
+        if (counter == 4)
+        {
+            PORTD &= ~((1 << PD5) | (1 << PD4) | (1 << PD3));
+            counter = 0;
+        }
+
+        if (!(PIND & (1 << PD6)))
+        {
+            counter++;
+            _delay_ms(200);
+            while (!(PIND & (1 << PD6)))
+                ;
+        }
+
+        switch (counter)
+        {
+        case 1:
+            PORTD |= (1 << PD5);
+            break;
+        case 2:
+            PORTD |= (1 << PD4);
+            break;
+        case 3:
+            PORTD |= (1 << PD3);
+            break;
+        }
+    }
+}
